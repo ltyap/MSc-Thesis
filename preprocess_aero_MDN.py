64990826be6142331c9d10605255bd1c376c3da8
ltyap <litoong.yap@gmail.com>
2023-02-15 04:00:39 +0100

    updated wcgan

--- preprocess_aero_MDN.py
+++ preprocess_aero_MDN.py
@@ -7,23 +7,30 @@ import collections
 # Pre-processing aero data
 import pandas as pd
 from sklearn import model_selection as model
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, Normalizer
 from misc_funcs import indexes
 
 
 LabelledData = collections.namedtuple("LabelledData",["x","y"])
-
 X_DIM = 3
 Y_DIM = 1
+# channel_name = 'TwrBsMyt_[kN-m] ST_DEL'
+channel_name = 'YawBrMyn_[kN-m] ST_DEL'
 
-df_train = pd.read_csv("datasets/aero/raw_data/train/data_raw.dat", header = 0, index_col = 0)
-df_test = pd.read_csv("datasets/aero/raw_data/test/data_raw.dat", header = 0, index_col = 0)
 
-aero_train = df_train[["URef", "PLExp", "IECturbc", "TwrBsMyt_[kN-m] ST_DEL"]]
-aero_test_raw = df_test.loc[:, ["URef", "PLExp", "IECturbc", "TwrBsMyt_[kN-m] ST_DEL"]]
+# df_train = pd.read_csv("datasets/aero_MDN/raw_data/train/data_raw.dat", header = 0, index_col = 0)
+# df_test = pd.read_csv("datasets/aero_MDN/raw_data/test/data_raw.dat", header = 0, index_col = 0)
+
+
+df_train = pd.read_csv("datasets/aero_MDN/raw_YawBrMyn_ST_DEL/train/data_raw.dat", header = 0, index_col = 0)
+df_test = pd.read_csv("datasets/aero_MDN/raw_YawBrMyn_ST_DEL/test/data_raw.dat", header = 0, index_col = 0)
+
+aero_train = df_train.loc[:,["URef", "PLExp", "IECturbc", channel_name]]
+aero_test_raw = df_test.loc[:, ["URef", "PLExp", "IECturbc", channel_name]]
 
 ss = StandardScaler().set_output(transform='pandas')
-# ss = MinMaxScaler(feature_range=(-1,1)).set_output(transform='pandas')
+# norm = Normalizer().set_output(transform='pandas')
+
 aero_train_scale = ss.fit_transform(aero_train)
 # Scale test data based on training data
 aero_test_scale = ss.transform(aero_test_raw)
@@ -35,17 +42,17 @@ np.savetxt("test.csv", aero_test_scale,delimiter=",")
 
 
 CHANNELS = ['URef','PLExp','IECturbc']
-PLOT_PATH = './plots/aero_TwrBsMyt_ST_DEL'
-PLOT_NAME = 'Scatter_Test_scaled'
+PLOT_PATH = 'plots/aero_MDN_YawBrMyn_ST_DEL'
+PLOT_NAME = 'Scatter_Test'
 # Initialise directory
 if not os.path.exists(PLOT_PATH):
     os.makedirs(PLOT_PATH)
 
 for channel in CHANNELS:
     plt.figure()
-    plt.scatter(aero_test_scale[channel],aero_test_scale["TwrBsMyt_[kN-m] ST_DEL"])
+    plt.scatter(df_test[channel],df_test[channel_name])
     plt.xlabel(channel)
-    plt.ylabel('TwrBsMyt_ST_DEL')
+    plt.ylabel(channel_name)
     plt.savefig(os.path.join(PLOT_PATH, '{}_{}'.format(PLOT_NAME,channel)))
     plt.close()
 
@@ -53,20 +60,21 @@ for channel in CHANNELS:
 test_scale = LabelledData(x= aero_test_scale.to_numpy()[:,:X_DIM],y = aero_test_scale.to_numpy()[:,X_DIM:])
 test_raw = LabelledData(x= aero_test_raw.to_numpy()[:,:X_DIM],y = aero_test_raw.to_numpy()[:,X_DIM:])
 
-
-listofx = np.linspace(0,14700,50, dtype=int)
+listofx = np.linspace(0,14400,49, dtype=int)
 PLT_TYPE = 'Test_PDF'
-testpdf_plot_path = os.path.join(PLOT_PATH,PLT_TYPE,'minmax')
-
+testpdf_plot_path = os.path.join(PLOT_PATH,PLT_TYPE)
+print(testpdf_plot_path)
 if not os.path.exists(testpdf_plot_path):
     os.makedirs(testpdf_plot_path)
 else:
     for f in os.listdir(testpdf_plot_path):
         os.remove(os.path.join(testpdf_plot_path,f))
+    print('Directory cleaned')
 assert os.path.exists(testpdf_plot_path),("dataset folder {} does not exist".format(testpdf_plot_path))
 
 for _, idx in enumerate(listofx):
     tmp = indexes(test_scale.x[idx], test_scale.x)
+    # print(tmp)
     plt.figure()
     sns.kdeplot(test_scale.y[tmp].squeeze(), color='k')
     plt.title('x={}, idx = {}'.format(test_raw.x[idx], idx), fontsize=10)
